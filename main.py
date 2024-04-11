466f18045d9f8fbef823c3807f5c73824c191ade
프로그래머스 교육팀 <soorte214@gmail.com>
2024-04-11 15:02:42 +0900

    함수 작성 : 더하기 add

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-nums = [1, 2]
-print(sum(nums))
+def add(num1, num2):
+    nums = [num1, num2]
+    print(sum(nums))
+
+
+
+# main
+if __name__=="__main__":
+    add(3, 5)
+    add(4, 6)
+    add(5, 7)
